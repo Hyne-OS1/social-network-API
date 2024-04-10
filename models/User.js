4b5eb26fa73f343b233e08ce0f22a234d3e0834d
Hyne-OS1 <146906218+Hyne-OS1@users.noreply.github.com>
2024-04-10 14:21:36 +1000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-// Importing the required dependencies from the mongoose library
+// Importing the required dependencies for mongoose
 const { Schema, model, Types } = require('mongoose'); 
-// Defining the User schema with the required fields and their respective data types
+
 const userSchema = new Schema(
   {
     username: {
@@ -9,7 +9,7 @@ const userSchema = new Schema(
         unique: true,
         trim: true,
     },
-      // using regular expression to validate email format
+      // using regex to validate email format (regular format)
     email: {
         type: String,
         required: true,
